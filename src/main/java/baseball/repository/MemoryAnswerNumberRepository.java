d243dc70aa0760b08c1bc95c00d88da843cbe8ad
Yomni <yearsheep@daum.net>
2022-10-03 11:01:21 +0900

    feat: Repository 정의
    Answer 객체를 생성 후 메모리상에서 가지고 있어야 하기 때문에 별도 MemoryAnswerNumberRepository를 정의하였습니다.

---
+++ src/main/java/baseball/repository/MemoryAnswerNumberRepository.java
@@ -0,0 +1,23 @@
+package baseball.repository;
+
+import baseball.domain.Answer;
+
+public class MemoryAnswerNumberRepository implements AnswerNumberRepository {
+
+    private Answer answer;
+
+    @Override
+    public void save(Answer answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public Answer findAnswerNumber() {
+        return this.answer;
+    }
+
+    @Override
+    public void clearStore() {
+        this.answer = null;
+    }
+}
